a8d97536d12603d71cc91a1fe2238a5476f415f7
jesusre890 <jesusre890@gmail.com>
2023-12-26 16:43:24 -0300

    searchbar

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export const MenuComponent: React.FC<{}>=() => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,13 @@
-import { Box, Container, Grid, Pagination } from "@mui/material";
+import { Box, Container, Grid, Pagination} from "@mui/material";
 import React from "react";
 import { HeaderComponent } from "../../components/Header";
 import { CardComponent } from "../../components";
+import SearchIcon from "@mui/icons-material/Search";
+
+import { styled } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import { MenuComponent } from "../../components/Menu";
+
 
 export const HomePage: React.FC<{}> = () => {
   const [page, setPage] = React.useState(1);
@@ -19,6 +25,51 @@ export const HomePage: React.FC<{}> = () => {
   //  endIndex
   //);
 
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    display: "flex",
+    justifyContent:"end",
+    borderRadius: 20,
+    backgroundColor: "#F86260",
+    "&:hover": {
+      backgroundColor: "#F86260",
+    },
+    marginTop:-40,
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    width: "100%",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
+        },
+      },
+    },
+  }));
+
   return (
     <Container maxWidth="xl">
       <>
@@ -28,7 +79,14 @@ export const HomePage: React.FC<{}> = () => {
             ml: { lg: 50, md: 30 },
           }}
         >
-          <HeaderComponent title="Platos" description="Opciones" />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase inputProps={{ "aria-label": "search" }} />
+          </Search>
+
+          <HeaderComponent title="Platos" description="" />
           <div>
             <Grid
               container
@@ -69,11 +127,12 @@ export const HomePage: React.FC<{}> = () => {
                 my: 3,
                 boxShadow: 5,
                 borderRadius: 5,
-                background: "#9e9e9e",
+                background: "#e0e0e0",
               }}
               size="large"
             />
           </Box>
+          <MenuComponent/>
         </Box>
       </>
     </Container>
